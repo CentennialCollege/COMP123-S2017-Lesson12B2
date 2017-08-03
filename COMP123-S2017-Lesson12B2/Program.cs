b9a6729529ff1da4d2dd71596020e3e54e0b72d8
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2017-08-03 12:41:12 -0400

    Created Calculator Project

--- COMP123-S2017-Lesson12B2/Program.cs
+++ COMP123-S2017-Lesson12B2/Program.cs
@@ -4,6 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+/*
+ * Name: Tom Tsiliopoulos
+ * Date: August 3, 2017
+ * Description: Calculator Demo Project
+ * Version: 0.1 - Created the Project
+ */
+
 namespace COMP123_S2017_Lesson12B2
 {
     static class Program
@@ -16,7 +23,7 @@ namespace COMP123_S2017_Lesson12B2
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new CalculatorForm());
         }
     }
 }
