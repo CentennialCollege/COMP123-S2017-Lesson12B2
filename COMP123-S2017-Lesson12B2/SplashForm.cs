f5088809bee566bd66b7f02ce1c13b90bb431316
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2017-08-08 16:45:16 -0400

    Added SplashForm

---
+++ COMP123-S2017-Lesson12B2/SplashForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+ * Name: Tom Tsiliopoulos
+ * Date: August 8, 2017
+ * Description: This is the SplashForm class
+ * Version: 0.1 - Created the SplashForm
+ */
+
+namespace COMP123_S2017_Lesson12B2
+{
+    public partial class SplashForm : Form
+    {
+        public SplashForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
